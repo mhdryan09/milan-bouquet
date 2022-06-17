9fd51854d14d178ae8ad090ae323c7199e059a9c
mhdryan09 <pranataryan91@gmail.com>
2022-06-17 09:47:45 +0700

    add homepage section

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,16 @@ $(document).ready(() => {
     $('.menu-wrapper').removeClass('active')
   });
 });
+
+var swiper = new Swiper(".mySwiperHome", {
+  // auto 1 per slide
+  slidesPerView: "auto",
+  // posisikan slide di tengah
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    delay: 2000
+  }
+});
